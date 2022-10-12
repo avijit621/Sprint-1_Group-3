8c8cd854a3b59a4a702415819538ab07820e4a9a
Biswajit637 <biswajitmishra637@gmail.com>
2022-10-12 12:40:35 +0530

    Add project files.

---
+++ OnlineFoodOrderingSystem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineFoodOrderingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ask the user credential
+             */
+            string userid, password;
+            Console.WriteLine("Enter UserID: ");
+            userid=Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            password=Console.ReadLine();
+            /*
+             * if id exists verify and proceed to show options otherwise redirect to create new userid
+             */
+            int choice;
+            do
+            {
+                Console.WriteLine()
+
+            } while (choice != 0);
+        }
+    }
+}
