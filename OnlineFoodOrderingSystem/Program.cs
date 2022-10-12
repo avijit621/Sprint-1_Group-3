0c227359fdec2ae3b2bd9b333f7961f058bc97df
Biswajit637 <biswajitmishra637@gmail.com>
2022-10-12 15:52:05 +0530

    Commit 4

--- OnlineFoodOrderingSystem/Program.cs
+++ OnlineFoodOrderingSystem/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
+using BLLayer;
+using EntityLayer;
 
 namespace OnlineFoodOrderingSystem
 {
