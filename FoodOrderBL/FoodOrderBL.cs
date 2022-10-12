f28acc8cb7503271ad082ac6653c21dc341fccfd
Biswajit637 <biswajitmishra637@gmail.com>
2022-10-13 01:58:06 +0530

    Commit 8

--- FoodOrderBL/FoodOrderBL.cs
+++ FoodOrderBL/FoodOrderBL.cs
@@ -10,5 +10,270 @@ namespace BLLayer
 {
     public class FoodOrderBL
     {
+        /*
+         * Customer Functionalities Business Logic
+         */
+        public Customers FindCustomers(string username)
+        {
+            Customers customer = new Customers();
+            try
+            {
+                // write logic here
+
+                //-----------------
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return customer;
+        }
+        public bool NewCustomerBL(Customers customer)
+        {
+            bool newcustadded = false;
+            try
+            {
+                // write logic here
+
+                //-----------------
+                newcustadded = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return newcustadded;
+        }
+        public bool UpdateDetailsBL(Customers customer)
+        {
+            bool updated = false;
+            try
+            {
+                // write logic here
+
+                //-----------------
+                updated = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return updated;
+        }
+        public bool ValidateExistingBL(Customers customer)
+        {
+            bool validuser = false;
+            try
+            {
+                //write logic here
+
+                //----------------
+                validuser = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return validuser;
+        }
+        public IList<OrderDetails> OrderDetailsBL(Customers customer)
+        {
+            IList<OrderDetails> list = new List<OrderDetails>();
+            try
+            {
+                // Write logic here
+
+                //------------------
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return list;
+        }
+        public IList<Payments> PaymentDetailsBL(Customers customer)
+        {
+            IList<Payments> list = new List<Payments>();
+            try
+            {
+                // Write logic here
+
+                //------------------
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return list;
+        }
+        public string TrackOrderBL(Customers customer)
+        {
+            string status="";
+            try
+            {
+                // write logic here
+
+                //-----------------
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return status;
+        }
+        public bool IsReadyForPayment(Customers customer)
+        {
+            bool ready = false;
+            try
+            {
+                //write logic here
+
+                //----------------
+                ready = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return ready;
+        }
+        public bool MakePaymentBL(Payments payments)
+        {
+            bool success = false;
+            try
+            {
+                //write logic here
+
+                //----------------
+                success = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return success;
+        }
+        public IList<FoodItems> ShowAllFoodItemsBL()
+        {
+            IList<FoodItems> list = new List<FoodItems>();
+            try
+            {
+                //write logic here
+
+                //----------------
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return list;
+        }
+        public IList<FoodItems> FilteredFoodItemsBL(FoodItems foodItems,string type)
+        {
+            IList<FoodItems> list = new List<FoodItems>();
+            try
+            {
+                //write logic here
+
+                //----------------
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return list;
+        }
+        public IList<FoodItems> AddToOrderBL(OrderDetails orderDetails)
+        {
+            IList<FoodItems> list = new List<FoodItems>();
+            try
+            {
+                //write logic here
+
+                //-----------------
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return list;
+        }
+        public bool IsAddressNullBL(Customers customer)
+        {
+            bool addressnull = false;
+            try
+            {
+                //write logic here
+
+                //----------------
+                addressnull = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return addressnull;
+        }
+        public bool EditAddressBL(Customers customer,string address)
+        {
+            bool addressedited = false;
+            try
+            {
+                //write logic here
+
+                //----------------
+                addressedited = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return addressedited;
+        }
+        public void PlaceOrderBL(Customers customer,OrderDetails orderDetails,IList<FoodItems> foodItemsList)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool ModifyOrderBL(OrderDetails orderDetails)
+        {
+            bool modified = false;
+            try
+            {
+                //write logic here
+
+                //----------------
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return modified;
+        }
+        public bool CancelOrderBL(OrderDetails orderDetails)
+        {
+            bool cancel = false;
+            try
+            {
+                //write logic here
+
+                //---------------
+                cancel = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return cancel;
+        }
+        /*
+         * Admin Fucntionalities Business Logic
+         */
     }
 }
--- OnlineFoodOrderingSystem/Program.cs
+++ OnlineFoodOrderingSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Remoting.Services;
 using System.Text;
@@ -73,17 +74,17 @@ namespace OnlineFoodOrderingSystem
                     case 1:
                         DisplayFoodMenu();break;
                     case 2:
-                        PlaceOrder();break;
+                        PlaceOrder(username);break;
                     case 3:
-                        ModifyOrder();break;
+                        ModifyOrder(username);break;
                     case 4:
-                        TrackOrder();break;
+                        TrackOrder(username);break;
                     case 5:
-                        MakePayment();break;
+                        MakePayment(username);break;
                     case 6:
-                        CheckPaymentStatus();break;
+                        ShowOrderDetails(username);break;
                     case 7:
-                        UpdateUserDetails();break;
+                        UpdateUserDetails(username);break;
                     case 8:
                         Logout();
                         choice = 0; break;
@@ -112,72 +113,79 @@ namespace OnlineFoodOrderingSystem
             }
         }
 
-        private static void UpdateUserDetails()
+        private static void MakePayment(string username)
         {
-            throw new NotImplementedException();
+            FoodOrderBL foodOrderBL = new FoodOrderBL();
+            Customers customer = foodOrderBL.FindCustomers(username);
+            Payments payments = new Payments();
+            payments.OrderId =customer.Orders.OrderID;
+            bool ready = foodOrderBL.IsReadyForPayment(customer);
+            if (ready)
+            {
+                try
+                {
+                    bool success = false;
+                    Console.WriteLine("Choose Payment Mode:\n\t1. Card\n\t2. UPI");
+                    int choice=Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter Card Number: ");
+                            payments.cardNo=Console.ReadLine();
+                            Console.WriteLine("Enter CVV: ");
+                            payments.CVV=Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter UPI Id: ");
+                            payments.UpiId=Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Choice.");
+                            break;
+                    }
+                    Console.WriteLine("Are you sure to proceed?");
+                    string ans=Console.ReadLine();
+                    if (string.Equals(ans, "y", StringComparison.OrdinalIgnoreCase) || string.Equals(ans, "yes", StringComparison.OrdinalIgnoreCase))
+                    {
+                        success = foodOrderBL.MakePaymentBL(payments);
+                    }
+                    if (success)
+                    {
+                        Console.WriteLine("Payment Successful. Order Placed.");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
         }
 
-        private static void CheckPaymentStatus()
+        private static void ShowOrderDetails(string username)
         {
-            throw new NotImplementedException();
-        }
-
-        private static void MakePayment()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void TrackOrder()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void ModifyOrder()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void PlaceOrder()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void DisplayFoodMenu()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        private static void CreateNewCustomer(string username, string password)
-        {
+            FoodOrderBL foodOrderBL = new FoodOrderBL();
+            Customers customer = new Customers();
+            customer =foodOrderBL.FindCustomers(username);
+            IList<OrderDetails> orderDetails = new List<OrderDetails>();
+            IList<Payments> paymentDetails = new List<Payments>(); 
             try
             {
-                Customer customer = new Customer();
-                FoodOrderBL foodOrderBL = new FoodOrderBL();
-                bool newcustadded = foodOrderBL.NewCustomerBL(username, password);
-                if (newcustadded)
+                orderDetails = foodOrderBL.OrderDetailsBL(customer);
+                Console.WriteLine($"{"OrderDate",-12}{"OrderId",-10}{"FoodItem",-20}{"Quantity",-10}{"Amount",-10}");
+                foreach (var data in orderDetails)                      // display order details
                 {
-                    Console.WriteLine("\nYour customer acccount created");
-                    Console.WriteLine("\nEnter few details to complete your profile**** ");
-                    Console.WriteLine("\nEnter Your Name: ");
-                    customer.Name = Console.ReadLine();
-                    Console.WriteLine("Enter Mobile Number: ");
-                    customer.Mobile=Console.ReadLine();
-                    Console.WriteLine("Enter Email address: ");
-                    customer.Email=Console.ReadLine();
-                    Console.WriteLine("Enter Gender: ");
-                    customer.Gender = Convert.ToChar(Console.ReadLine());
-                    Console.WriteLine("Enter City: ");
-                    customer.City=Console.ReadLine();
-                    Console.WriteLine("Enter Pincode: ");
-                    customer.Pincode=Console.ReadLine();
-                    Console.WriteLine("Enter Food Delivery Address");
-                    customer.Address = Console.ReadLine();
-                    bool updated = foodOrderBL.UpdateDetailsBL(customer);
-                    if (updated)
+                    Console.WriteLine($"{data.Orders.OrderDate,-12}{data.OrderID,-10}{data.FoodItem.Name,-20}{data.Quantity,-10}{data.Quantity*data.UnitPrice*(1-data.Discount/100),-10}");
+                }
+                Console.WriteLine("Do you want to see the payment details?");
+                string ans=Console.ReadLine();
+                if(string.Equals(ans, "y", StringComparison.OrdinalIgnoreCase) || string.Equals(ans, "yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    paymentDetails=foodOrderBL.PaymentDetailsBL(customer);
+                    Console.WriteLine($"{"OrderDate",-12}{"OrderId",-10}{"TransactionId",-10}{"PayMode",-10}{"Card",-12}{"UPI",-12}{"Amount",-10}");
+                    foreach (var data in paymentDetails)                    // display payment details
                     {
-                        Console.Clear();
-                        Console.WriteLine("Your profile is updated\n");
+                        Console.WriteLine($"{data.Orders.OrderDate,-12}{data.OrderId,-10}{data.PaymentID,-10}{data.PayType,-10}{data.cardNo,-12}{data.UpiId,-12}{data.Amount,-10}");
                     }
                 }
             }
@@ -187,9 +195,256 @@ namespace OnlineFoodOrderingSystem
             }
         }
 
-        private static bool ValidateExistingCustomer(string userid, string password)
+        private static void TrackOrder(string username)
         {
-            return true;
+            Customers customer = new Customers();
+            customer.UserName = username;
+            FoodOrderBL foodOrderBL = new FoodOrderBL();
+            try
+            {
+                string status = $"{foodOrderBL.TrackOrderBL(customer)}";
+                Console.WriteLine($"\n{status}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ModifyOrder(string username)
+        {
+            FoodOrderBL foodOrderBL = new FoodOrderBL();
+            Customers customer=foodOrderBL.FindCustomers(username);
+            OrderDetails orderDetails = new OrderDetails();
+            try
+            {
+                Console.WriteLine("Do you want to modify address?");
+                string ans=Console.ReadLine();
+                if (string.Equals(ans, "y", StringComparison.OrdinalIgnoreCase) || string.Equals(ans, "yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Enter Address: ");
+                    string addr=Console.ReadLine();
+                    bool edited=foodOrderBL.EditAddressBL(customer, addr);
+                    if (edited)
+                    {
+                        Console.WriteLine("Address updated");
+                    }
+                }
+                Console.WriteLine("Do you want to modify fooditems?");
+                ans = Console.ReadLine();
+                if (string.Equals(ans, "y", StringComparison.OrdinalIgnoreCase) || string.Equals(ans, "yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Enter the foodItem ID: ");
+                    orderDetails.FoodID = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Quantity: ");
+                    orderDetails.Quantity=Convert.ToInt32(Console.ReadLine());
+                    bool updated=foodOrderBL.ModifyOrderBL(orderDetails);
+                    if (updated)
+                    {
+                        Console.WriteLine("Order updated");
+                    }
+                }
+                Console.WriteLine("Do you want to cancel Order?");
+                ans=Console.ReadLine();
+                if (string.Equals(ans, "y", StringComparison.OrdinalIgnoreCase) || string.Equals(ans, "yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    bool canceled = foodOrderBL.CancelOrderBL(orderDetails);
+                    if (canceled)
+                    {
+                        Console.WriteLine("Order Cancelled....");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void PlaceOrder(string username)
+        {
+            FoodOrderBL foodOrderBL=new FoodOrderBL();
+            Customers customer=foodOrderBL.FindCustomers(username);
+            OrderDetails orderDetails=new OrderDetails();
+            IList<FoodItems> foodItemsList=new List<FoodItems>();
+            try
+            {
+                Console.Clear();
+                DisplayFoodMenu();
+                int choice = 1;
+                do
+                {
+                    Console.WriteLine("\n\nDo you want to add fooditem to order?\n\t1. Yes\n\t0. No");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\nEnter FoodItem ID to add: ");
+                    orderDetails.FoodID = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter quantity of the above fooditem: ");
+                    orderDetails.Quantity = Convert.ToInt32(Console.ReadLine());
+                    foodItemsList=foodOrderBL.AddToOrderBL(orderDetails);
+                } while (choice != 0);
+                bool addressnull = foodOrderBL.IsAddressNullBL(customer);
+                if (addressnull)
+                {
+                    Console.WriteLine("Enter Delivery Address:");
+                    string address=Console.ReadLine();
+                    bool addressupdated =foodOrderBL.EditAddressBL(customer, address);
+                    if (addressupdated)
+                    {
+                        Console.WriteLine("Delivary Address Updated");
+                    }
+                }
+                Console.WriteLine("Do you want to confirm the order and proceed to payment?");
+                string ans=Console.ReadLine();
+                if (string.Equals(ans, "y", StringComparison.OrdinalIgnoreCase) || string.Equals(ans, "yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    foodOrderBL.PlaceOrderBL(customer,orderDetails,foodItemsList);
+                    MakePayment(username);
+                }
+                else
+                {
+                    ModifyOrder(username);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void DisplayFoodMenu()
+        {
+            FoodOrderBL foodOrderBL = new FoodOrderBL();
+            FoodItems foodItems = new FoodItems();
+            IList<FoodItems> foodItemsList = new List<FoodItems>();
+            try
+            {
+                Console.Clear();
+                foodItemsList=foodOrderBL.ShowAllFoodItemsBL();
+                DisplayFoodItems(foodItemsList);
+                Console.WriteLine("Choose Food Type:\n\t1. Veg\n\t2. Non-Veg\n\t3. Vegan");
+                int choice1 = Convert.ToInt32(Console.ReadLine());
+                string type = "";
+                switch (choice1)
+                {
+                    case 1:
+                        type = "Veg";
+                        foodItemsList = foodOrderBL.FilteredFoodItemsBL(foodItems,type);
+                        DisplayFoodItems(foodItemsList);
+                        break;
+                    case 2:
+                        type = "Non-Veg";
+                        foodItemsList = foodOrderBL.FilteredFoodItemsBL(foodItems,type);
+                        DisplayFoodItems(foodItemsList);
+                        break;
+                    case 3:
+                        type = "Vegan";
+                        foodItemsList = foodOrderBL.FilteredFoodItemsBL(foodItems, type);
+                        DisplayFoodItems(foodItemsList);
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid choice.");
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void DisplayFoodItems(IList<FoodItems> foodItemsList)
+        {
+            try
+            {
+                if(foodItemsList == null)
+                {
+                    throw new Exception($"No Food Items Available of such type");
+                }
+                Console.WriteLine($"{"FoodID",-10}{"FoodName",-20}{"FoodPrice",-8}{"Category",-10}{"Restaurant",-15}{"Type",-10}");
+                foreach (var data in foodItemsList) 
+                {
+                    Console.WriteLine($"{data.FoodID,-10}{data.Name,-20}{data.Price,-8}{data.Category,-10}{data.Restaurant,-15}{data.Type,-10}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void UpdateUserDetails(string username)
+        {
+            Customers customer = new Customers();
+            FoodOrderBL foodOrderBL = new FoodOrderBL();
+            customer=foodOrderBL.FindCustomers(username);
+            try
+            {
+                Console.WriteLine("\nYour customer acccount created");
+                Console.WriteLine("\nEnter few details to complete your profile **** ");
+                Console.WriteLine("\nEnter Your Name: ");
+                customer.Name = Console.ReadLine();
+                Console.WriteLine("Enter Mobile Number: ");
+                customer.Mobile = Console.ReadLine();
+                Console.WriteLine("Enter Email address: ");
+                customer.Email = Console.ReadLine();
+                Console.WriteLine("Enter Gender: ");
+                customer.Gender = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Enter City: ");
+                customer.City = Console.ReadLine();
+                Console.WriteLine("Enter Pincode: ");
+                customer.Pincode = Console.ReadLine();
+                Console.WriteLine("Enter Food Delivery Address");
+                customer.Address = Console.ReadLine();
+                bool updated = foodOrderBL.UpdateDetailsBL(customer);
+                if (updated)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Your profile is updated\n");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void CreateNewCustomer(string username, string password)
+        {
+            Customers customer = new Customers();
+            customer.UserName = username;
+            customer.Password= password;
+            FoodOrderBL foodOrderBL = new FoodOrderBL();
+            try
+            {
+                bool newcustadded = foodOrderBL.NewCustomerBL(customer);
+                if (newcustadded)
+                {
+                    UpdateUserDetails(username);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static bool ValidateExistingCustomer(string username, string password)
+        {
+            bool valid = false;
+            try
+            {
+                Customers customer = new Customers();
+                customer.UserName = username;
+                customer.Password = password;
+                FoodOrderBL foodOrderBL = new FoodOrderBL();
+                valid = foodOrderBL.ValidateExistingBL(customer);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return valid;
         }
 
         private static void DisplayChoice()
